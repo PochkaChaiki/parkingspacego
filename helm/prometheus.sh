a926b8fc15ebd20c3e4ef943ecb632a24cda4fc7
PochkaChaiki <pochka.chaiki@gmail.com>
2026-05-20 11:55:40 +0300

    added argocd deployment and improved mongo secret usage

--- helm/prometheus.sh
+++
@@ -1,2 +0,0 @@
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace --set graphana.adminPassword=admin123 --set prometheus.prometheusSpec.
-serviceMonitorSelectorNilUsesHelmValues=false
